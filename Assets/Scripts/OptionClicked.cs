47a7697214348bcaae2638e1a9daec03c901b866
GoodSSenDev <yoodonggeun7@naver.com>
2020-04-25 04:03:18 +1200

    for checkout

---
+++ Assets/Scripts/OptionClicked.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionClicked : MonoBehaviour
+{
+    private MyMenuButton menuButton;
+    public GameObject frame;
+    public GameObject inView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuButton = GetComponent<MyMenuButton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(menuButton.name == "Exit")
+        {
+            Application.Quit();
+        }
+
+        if (menuButton.clicked)
+        {
+            GobalFrame.bringToView(frame,menuButton);
+        }
+        else
+        {
+            GobalFrame.leaveView(frame);
+        }
+
+        inView = GobalFrame.frameInView;
+
+    }
+}

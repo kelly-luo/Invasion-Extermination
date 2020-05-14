5b8b8aaa892ff1f3f59d27f638651d78f22d694d
kelly-luo <luo.kelly@hotmail.com>
2020-05-14 18:48:15 +1200

    Beginning to write the unit tests

---
+++ Assets/Test/US8Tests_SaveLoad/SaveLoadTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace US8Tests_SaveLoad
+{
+
+    public class SaveLoadTest
+    {
+
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+
+        private game;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("03.Prefabs/Characters/TestPlayer"));
+            game = gameGameObject.GetComponent<Game>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator AsteroidsMoveDown()
+        {
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            float initialYPos = asteroid.transform.position.y;
+            yield return new WaitForSeconds(0.1f);
+
+            Assert.Less(asteroid.transform.position.y, initialYPos);
+        }
+
+        // Test if the save file exists
+
+        // Test if the loaded character has correct position (x,y,z)
+
+        // Test if the loaded character has the correct health points
+
+    }
+}

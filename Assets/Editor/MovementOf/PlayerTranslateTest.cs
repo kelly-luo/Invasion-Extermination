7d2623bc2bef80db44767a1202b8a7fce1d265cd
Thilan Shiroyuki Liyanage <Shiroyuki-Liyanage@users.noreply.github.com>
2020-05-16 22:25:50 +1200

    Added Crosshair

--- Assets/Editor/MovementOf/PlayerTranslateTest.cs
+++ Assets/Editor/MovementOf/PlayerTranslateTest.cs
@@ -28,10 +28,10 @@ namespace Tests
         public void TranslateCharacter_MovingTest()
         {
             this.SetupTest();
-            
+
             playerTranslate.TranslateCharacter(new Vector3(1f, 0f, 0f));
 
-            var ExpectedValue = new Vector3(1.5f, 0f,0f);
+            var ExpectedValue = new Vector3(1.5f, 0f, 0f);
 
             Assert.AreEqual(ExpectedValue, target.transform.position);
         }

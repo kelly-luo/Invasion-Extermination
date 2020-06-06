eae4987b90df19c84a955e0eb6899c18c4bbdfd3
kelly-luo <luo.kelly@hotmail.com>
2020-06-06 22:08:10 +1200

    Finished writing the unit tests

--- Assets/02.Scripts/UI-HUD/bcSave.cs
+++ Assets/02.Scripts/UI-HUD/bcSave.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class bcSave : ButtonClicked
 {
     [SerializeField] private PlayerInformation playerInformation;
+    [SerializeField] private TMP_Text saveLoadText;
     public override void ButtonEvent(PointerEventData eventData)
     {
         playerInformation.SavePlayer();
@@ -13,11 +15,17 @@ public class bcSave : ButtonClicked
 
     public override void ButtonHover(PointerEventData eventData)
     {
-        
+
     }
 
     public override void ButtonHoverExit(PointerEventData eventData)
     {
-   
+
+    }
+
+    public void DisplaySaveLoadText()
+    {
+        //float startTime = UnityService.DeltaTime;
+        //saveLoadText.gameObject.SetActive(false);
     }
 }
---
+++ Assets/Editor/SaveLoad/US49SaveLoadTextTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using TMPro;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    //
+    // US49SaveLoadTextTest
+    // ~~~~~~~~~~~~~~~~~~~~
+    // This test class is used to test user story 49: As a player I want to have a UI pop up so that I know I have completed my save or load successfully
+    // 
+    // AUT University - 2020 - Kelly Luo
+    // 
+    // Revision History
+    // ~~~~~~~~~~~~~~~~
+    // 5.06.2020 Creation date
+    //
+    public class US49SaveLoadTextTest
+    {
+        GameObject saveText;
+        GameObject loadText;
+
+        bcSave saveButton;
+        bcLoad loadButton;
+
+        [SetUp]
+        public void SetUp()
+        {
+            saveText = new GameObject();
+            saveText.AddComponent<TextMeshPro>();
+
+            loadText = new GameObject();
+            loadText.AddComponent<TextMeshPro>();
+
+            saveButton = new bcSave();
+            loadButton = new bcLoad();
+        }
+
+        // This test will check if the save completed text is not visible before saving button is pressed
+        [Test]
+        public void US49SaveLoadText_TestIfTextIsNotActiveBeforeSave()
+        {
+            Assert.IsFalse(saveText.activeSelf);
+        }
+
+        // This test will check if the save completed text is active (has appeared) after saving button is pressed
+        [Test]
+        public void US49SaveLoadText_TestIfTextIsActiveAfterSave()
+        {
+            saveButton.ButtonEvent(null);
+            Assert.IsTrue(saveText.activeSelf);
+        }
+
+        // This test will check if the load completed text is not visible before load button is pressed
+        [Test]
+        public void US49SaveLoadText_TestIfTextIsNotActiveBeforeLoad()
+        {
+            Assert.IsFalse(loadText.activeSelf);
+        }
+
+        // This test will check if the load completed text is active (has appeared) after load button is pressed
+        [Test]
+        public void US49SaveLoadText_TestIfTextIsActiveAfterLoad()
+        {
+            loadButton.ButtonEvent(null);
+            Assert.IsTrue(loadText.activeSelf);
+        }
+
+
+    }
+}
